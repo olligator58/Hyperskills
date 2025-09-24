a2e3d960d20e6ecc7184a92d25cb191a6b16096f
Oleg <olligator58@gmail.com>
2025-09-24 17:25:18 +0300

    Overloading

---
+++ Topics/Overloading/Print/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void print(String strArg) {
+        System.out.println("print(\"" + strArg + "\")");
+    }
+
+    public static void print(String str, int num) {
+        System.out.println("print(\"" + str + "\", " + num + ")");
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int val = scanner.nextInt();
+        print(str);
+        print(str, val);
+    }
+}
