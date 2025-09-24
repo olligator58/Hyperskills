acca44baaae39df7add8f33d66d3f2a1b95f6bc7
Oleg <olligator58@gmail.com>
2025-09-24 10:15:49 +0300

    Array

---
+++ Topics/Array/Sum of multiples of 3 in an array/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (numbers[i] % 3 == 0) {
+                sum += numbers[i];
+            }
+        }
+        System.out.println(sum);
+        scanner.close();
+    }
+}
