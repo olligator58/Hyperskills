46e0a4539b60fd89bfdee30334d1bfd4ddcf0abf
Oleg <olligator58@gmail.com>
2025-11-06 15:34:27 +0300

    Reading files

--- Topics/File class/Working with file data in Java/src/Task.java
+++ Topics/File class/Working with file data in Java/src/Task.java
@@ -1,7 +1,23 @@
 // You can experiment here, it won’t be checked
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task {
-  public static void main(String[] args) {
-    // put your code here
-  }
+    public static void main(String[] args) {
+        String fileName = "C:\\Temp\\flud\\dataset_91007.txt";
+        int max = 0;
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNext()) {
+                int number = scanner.nextInt();
+                if (number > max) {
+                    max = number;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Такого файла не существует !!!");
+        }
+        System.out.println(max);
+    }
 }
