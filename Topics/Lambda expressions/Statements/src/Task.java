b392aabb4cc7daa3cd6d4e79742650ed706336f1
Oleg <olligator58@gmail.com>
2025-11-24 15:27:28 +0300

    Method references

--- Topics/Lambda expressions/Statements/src/Task.java
+++ Topics/Lambda expressions/Statements/src/Task.java
@@ -1,7 +1,28 @@
-// You can experiment here, it won’t be checked
+import java.util.function.Supplier;
 
 public class Task {
-  public static void main(String[] args) {
-    // put your code here
-  }
+    public static void main(String[] args) {
+        Customer customer = new Customer("Paul", "Morris");
+        Supplier<String> getName = customer::getFirstName;
+        System.out.println(getName.get());
+    }
+}
+
+class Customer {
+
+    String firstName;
+    String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
